89e6de719502cd36905e7f308e0f34d2656fbc16
LunaRenilson <renilson.c.luna@gmail.com>
2025-07-09 12:51:02 -0300

    directories fixed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 // https://vite.dev/config/
+import { defineConfig } 
+from "vite";
+
+import react from "@vitejs/plugin-react";
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/portfolio",
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  
+  preview: {
+    port: 5173,
+    strictPort: true,
+  },
+
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:5173",
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+    },
+  },
+});
+
