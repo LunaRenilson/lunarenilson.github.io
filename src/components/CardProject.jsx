fd02d4ae3d0b6ec6ce39e42399c0fd2fcb05e1e8
LunaRenilson <renilson.c.luna@gmail.com>
2025-07-12 07:32:15 -0300

    two featured projects added

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -1,13 +1,13 @@
 import tags from "@data/tagsProject.json";
 import { Link } from "react-router-dom";
 const CardProject = ({ project }) => {
-  const images = import.meta.glob("@assets/*", {
+  const images = import.meta.glob("@assets/project_images/*", {
     eager: true,
     import: "default",
   });
 
   const image =
-    images[`/src/assets/${project.images[0]}`] || images[`@assets/default.png`];
+    images[`/src/assets/project_images/${project.images[0]}`] || images[`@assets/default.png`];
 
   return (
     <div className="border border-gray-200 rounded-lg w-1/5 flex-wrap shadow-md hover:shadow-2xl hover:border-blue-300 active:scale-95 overflow-hidden bg-white">
@@ -21,12 +21,12 @@ const CardProject = ({ project }) => {
           <h3 className="mb-2 text-lg font-semibold">{project.title}</h3>
           <p className="mb-3 text-gray-600">{project.summary}</p>
           <ul className="inline-block text-gray-800 rounded px-2 py-1 text-xs">
-            {project.tags.map((tagId, idx) => (
+            {project.tags.map((tag, idx) => (
               <li
                 key={idx}
                 className="inline-block mr-2 mb-1 bg-blue-200 text-blue-800 px-2 py-1 rounded-full"
               >
-                {tags[tagId] ? tags[tagId].tag : `Tag ${tagId}`}
+                {tag}
               </li>
             ))}
           </ul>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -11,16 +11,16 @@ export default function Projects() {
       <h1 className="text-4xl ml-10 font-bold mb-4">Projects</h1>
       <section className="ml-10 pb-5 w-1/2 flex flex-col border-b-1 border-gray-500">
         <div className="flex gap-2 items-baseline mb-4">
-          <p className="mb-2">Search by Tag</p>
+          <p className="mb-2">Search</p>
           <input
             type="text"
             className="border-1 h-7 border-gray-300 rounded-sm outline-none p-5"
-            placeholder="Search tag"
+            placeholder="Search tag, title or description"
           />
         </div>
 
         <ul className="flex flex-wrap gap-2 justify-start">
-          {tags.map(({ id, tag }, idx) => (
+          {tags.map((tag, idx) => (
             <li
               key={idx}
               className="bg-blue-200 text-blue-800 px-3 py-1 rounded-full text-md"
@@ -31,7 +31,7 @@ export default function Projects() {
         </ul>
       </section>
 
-      <section className=" pl-10  bg-gray-300 py-5">
+      <section className=" pl-10  bg-gray-300 pb-5">
         <div className="flex gap-10 flex-wrap justify-start mt-10">
           {projects.map((project, idx) => (
               <CardProject key={idx} project={project} />
